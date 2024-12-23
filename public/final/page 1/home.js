13585386a6d8874227090a7fb57d7044aaff34b1
Osaid-Mtawi <s12341143@stu.najah.edu>
2024-12-23 18:54:23 +0200

    change js

--- public/final/page 1/home.js
+++ public/final/page 1/home.js
@@ -3,15 +3,11 @@
     const totalImages = images.length;
 
     function changeImage() {
-        // إزالة الفئة "active" من الصورة الحالية
         images[currentIndex].classList.remove('active');
         
-        // تحديد الصورة التالية
         currentIndex = (currentIndex + 1) % totalImages;
         
-        // إضافة الفئة "active" للصورة التالية
         images[currentIndex].classList.add('active');
     }
 
-    // تغيير الصورة كل 3 ثواني
     setInterval(changeImage, 3000);
