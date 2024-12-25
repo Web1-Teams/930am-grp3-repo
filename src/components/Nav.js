3b4386930bdc8e850ff273c3eabcf4e78572b9e6
Osaid-Mtawi <s12341143@stu.najah.edu>
2024-12-25 17:57:49 +0200

    add react

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
 //import
-import logo from "../assests/images/Picsart_24-12-11_00-26-28-373.jpg";
+import logo from "../assest/icons/images/Picsart_24-12-11_00-26-28-373.jpg"
 import '../styles/nav.css';
 //function
 const Nav=()=>{
@@ -12,24 +12,23 @@ const Nav=()=>{
                 </div>
                 <ul className="navigation">
                     <li className="menu-item-10"><a title="Home" href="home.html">Home</a></li>
-                    <li className="menu-item-11"><a title="About" href="..//page 5/about.html">Abouttt</a>
+                    <li className="menu-item-11"><a title="About" href="..//page 5/about.html">About</a>
                         <ul role="menu" className="submenu">
                             <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1"><a href="..//page 5/leadership.html">Contact Us</a></li>
                         </ul>
                     </li>
-                    <li className="menu-item-12"><a title="Animal Care" href="home.html">Animal Care</a>
+                    <li className="menu-item-12"><a title="Animal Care" href="..//page 3/community.html">Our Community</a>
                         <ul role="menu" className="submenu">
-                            <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-2"><a href="home.html">Advice</a></li>
-                            <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-3"><a href="home.html">Health</a></li>
-                            <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-4"><a href="home.html">Articles</a></li>
+                            <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-2"><a href="..//page 3/community.html">Login</a></li>
+                            <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-3"><a href="..//page 3/community.html">Articles</a></li>
+                            <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-4"><a href="..//page 3/community.html">Questions</a></li>
                         </ul>
                     </li>
-                    <li className="menu-item-13"><a title="Support" href="home.html">Support</a></li>
-                    <li className="menu-item-14"><a title="Volunteer" href="home.html">Volunteer</a></li>
+                    <li className="menu-item-13"><a title="Support" href="..//page 4/resuce.html">Found</a></li>
+                    <li className="menu-item-14"><a title="Volunteer" href="..//page 6/volunter.html">Volunteer</a></li>
                     <li className="menu-item-15"><a title="Report" href="home.html">Report</a></li>
-                    <li className="menu-item-16"><a title="Login" href="home.html">Login</a></li>
-                    <button className="Donate">
-                        <span>Donate</span>
+                    <button className="Donatee">
+                        <a href="..//page 6/donate.html"><span>Donate</span></a>
                     </button> 
                 </ul>
             </div>
