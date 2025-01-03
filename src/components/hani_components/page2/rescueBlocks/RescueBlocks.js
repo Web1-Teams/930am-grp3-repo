1d8e5dbbf3380fc9a50cbb54ab53d19778487693
Hani Basha <s12323160@stu.najah.edu>
2025-01-03 20:43:00 +0200

    css and add a local storage

--- src/components/hani_components/page2/rescueBlocks/RescueBlocks.js
+++ src/components/hani_components/page2/rescueBlocks/RescueBlocks.js
@@ -1,13 +1,19 @@
-import React from "react";
 import "./RescueBlocks.css"
 const Block=(props)=>{
     const typeTitle= (props.type) ? props.type : "text";
     const classes= (props.className) ? props.className : "non";  
+
+
+    const handleChange = (event) => {
+        localStorage.setItem(props.title, event.target.value); 
+      };
+
+      
     return(
         <>
     <div id="input" className="absulute">
         <lable for="type"> {props.title} <br/> </lable>
-        <input id="type" type={typeTitle} className={classes}  placeholder={props.placeholder} />
+        <input id="type" type={typeTitle} className={classes}  placeholder={props.placeholder}   onChange={handleChange}/>
         <br/>
     </div>
     </>
